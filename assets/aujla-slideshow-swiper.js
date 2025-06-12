84ec4adb65bb817806f10b65ea42dad9a31e6e20
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-06-12 13:17:48 +0530

    SlideShow Created using Swiper JS

---
+++ assets/aujla-slideshow-swiper.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
