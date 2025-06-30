03db6f48da26de36fd1802532ffc7249467800e2
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-06-30 13:14:33 +0530

    Mini Slideshow and Full slideshow updated

--- assets/aujla-slideshow-swiper.js
+++ assets/aujla-slideshow-swiper.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const swiperEl = document.querySelector('.mini-slideshow-left');
+  const swiperEl = document.querySelector('.mySwiper');
   const autoplayDelay = parseInt(swiperEl.dataset.autoplayDelay) || 4000;
   new Swiper(swiperEl, {
     loop: true,
